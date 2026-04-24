c3ee0d5896edbb4e2ce15818168b122c80e72c04
abrilweinbaur <asilvaweinbaur@udesa.edu.ar>
2026-04-24 01:49:33 -0300

    Scraping completo OEA: implementación para enero–abril con funciones

--- TP2/scripts/scraping_oea.R
+++ TP2/scripts/scraping_oea.R
@@ -4,6 +4,8 @@
 # Script para scrapear comunicados de la OEA
 ######################### 
 
+## En el segundo commit está la versión de prueba con el mes abril
+
 #Instalar librerias
 #install.packages("here")
 
@@ -24,88 +26,15 @@ if (!dir.exists(here("TP2", "data"))) {
   message("Carpeta /data ya existe")
 }
 
-# Definir URL de prueba: comunicados de abril de 2026
-url_abril <- "https://www.oas.org/es/centro_noticias/comunicados_prensa.asp?nMes=4&nAnio=2026"
-message("Leyendo página de comunicados de abril de 2026...")
+# Definir meses a scrapear
 
-# Leer el HTML de la página
-pagina_abril <- read_html(url_abril)
-message("Página de abril leída correctamente")
-
-# Guardar una copia del HTML descargado
-fecha_descarga <- Sys.Date() #Guarda la fecha del día en que corres el script
-
-write_html(
-  pagina_abril,
-  file = file.path(
-    here("TP2", "data"),
-    paste0("oea_comunicados_abril_2026_", fecha_descarga, ".html")
-  )
+meses <- tibble(
+  numero_mes = c(1, 2, 3, 4),
+  nombre_mes = c("enero", "febrero", "marzo", "abril")
 )
-message("HTML de abril guardado en la carpeta /data")
-
-## PRUEBA
-#Extraer títulos 
-#message("Extrayendo títulos de comunicados con SelectorGadget...")
-
-#titulos_abril <- pagina_abril |>
-#  html_elements("#rightmaincol td:nth-child(2)") |>
-#html_text2()
-
-#head(titulos_abril)
-
-#Extraer links
-#links_abril <- pagina_abril |>
-#  html_elements("a[href*='comunicado_prensa']") |>
-#  html_attr("href")
-
-#head(links_abril)
-
-#links_abril_completos <- links_abril |>
-#  url_absolute(url_abril)
-
-#head(links_abril_completos)
-
-# Crear una tabla inicial con títulos y links de abril
-
-nodos_abril <- pagina_abril |>
-  html_elements("#rightmaincol td:nth-child(2) a")
-
-titulos_abril <- nodos_abril |>
-  html_text2()
-
-links_abril <- nodos_abril |>
-  html_attr("href")
-
-links_abril_completos <- links_abril |>
-  url_absolute(url_abril)
-
-tabla_abril <- tibble(
-  id = seq_along(titulos_abril),
-  titulo = titulos_abril,
-  link = links_abril_completos
-)
-
-head(tabla_abril)
-
-## PRUEBA
-# Probar extracción de un comunicado individual
-#url_prueba <- tabla_abril$link[1]
-#message("Leyendo comunicado de prueba: ", url_prueba)
-
-#pagina_comunicado <- read_html(url_prueba)
-#message("Comunicado de prueba leído correctamente")
-
-#pagina_comunicado
-
-# Extraer cuerpo del comunicado de prueba con SelectorGadget
-#cuerpo_prueba <- pagina_comunicado |>
-#  html_elements("p:nth-child(5)") |>
-#  html_text2()
-
-#cuerpo_prueba
 
 # Función para extraer el cuerpo de un comunicado
+
 extraer_cuerpo <- function(link) {
   
   message("Leyendo comunicado: ", link)
@@ -118,7 +47,7 @@ extraer_cuerpo <- function(link) {
     html_elements("p:nth-child(5), p:nth-child(6)") |> 
     #Seleccionar (5) y (6), ya que cuando no muestra imagen es (5) pero cuando muestra una imagen al principio de la página es (6)
     html_text2()
-
+  
   # Siempre unir en un único string
   if (length(cuerpo) == 0) {
     return(NA_character_)
@@ -126,28 +55,92 @@ extraer_cuerpo <- function(link) {
     return(paste(cuerpo, collapse = " "))
   }
 }
+
+# Función para scrapear un mes completo
+
+scrapear_mes <- function(numero_mes, nombre_mes) {
   
-extraer_cuerpo(tabla_abril$link[1])
+  # Definir URL del mes
+  url_mes <- paste0(
+    "https://www.oas.org/es/centro_noticias/comunicados_prensa.asp?nMes=",
+    numero_mes,
+    "&nAnio=2026"
+  )
+  
+  message("Leyendo página de comunicados de ", nombre_mes, " de 2026...")
+  
+  # Leer el HTML de la página
+  pagina_mes <- read_html(url_mes)
+  message("Página de ", nombre_mes, " leída correctamente")
+  
+  # Guardar una copia del HTML descargado
+  fecha_descarga <- Sys.Date() #Guarda la fecha del día en que corres el script
+  
+  write_html(
+    pagina_mes,
+    file = file.path(
+      here("TP2", "data"),
+      paste0("oea_comunicados_", nombre_mes, "_2026_", fecha_descarga, ".html")
+    )
+  )
+  
+  message("HTML de ", nombre_mes, " guardado en la carpeta /data")
+  
+  # Crear una tabla inicial con títulos y links del mes
+  
+  nodos_mes <- pagina_mes |>
+    html_elements("#rightmaincol td:nth-child(2) a")
+  
+  titulos_mes <- nodos_mes |>
+    html_text2()
+  
+  links_mes <- nodos_mes |>
+    html_attr("href")
+  
+  links_mes_completos <- links_mes |>
+    url_absolute(url_mes)
+  
+  tabla_mes <- tibble(
+    id = paste0(nombre_mes, "_", seq_along(titulos_mes)),
+    titulo = titulos_mes,
+    link = links_mes_completos
+  )
+  
+  head(tabla_mes)
+  
+  # Extraer cuerpo de cada comunicado
+  
+  tabla_mes$cuerpo <- map_chr(tabla_mes$link, extraer_cuerpo)
+  
+  sum(is.na(tabla_mes$cuerpo)) #Chequea si hay NAs
+  
+  head(tabla_mes)
+  
+  # Dejar solamente las variables solicitadas por la consigna
+  
+  tabla_mes_final <- tabla_mes |>
+    select(id, titulo, cuerpo)
+  
+  return(tabla_mes_final)
+}
 
-tabla_abril$cuerpo <- map_chr(tabla_abril$link, extraer_cuerpo)
+# Scrapear enero, febrero, marzo y abril de 2026
 
-sum(is.na(tabla_abril$cuerpo)) #Chequea si hay NAs
-
-head(tabla_abril)
-
-# Dejar solamente las variables solicitadas por la consigna
-
-tabla_abril_final <- tabla_abril |>
-  select(id, titulo, cuerpo)
-
-head(tabla_abril_final)
-
-# Guardar tabla final de abril
-
-saveRDS(
-  tabla_abril_final,
-  file = here("TP2", "data", "comunicados_oea_abril_2026.rds")
+tabla_final <- map2_dfr(
+  meses$numero_mes,
+  meses$nombre_mes,
+  scrapear_mes
 )
 
-message("Tabla de abril guardada correctamente en /data")
+sum(is.na(tabla_final$cuerpo)) #Chequea si hay NAs
 
+head(tabla_final)
+
+# Guardar tabla final de enero, febrero, marzo y abril
+
+saveRDS(
+  tabla_final,
+  file = here("TP2", "data", "comunicados_oea_enero_abril_2026.rds")
+)
+
+message("Tabla final de enero, febrero, marzo y abril guardada correctamente en /data")
